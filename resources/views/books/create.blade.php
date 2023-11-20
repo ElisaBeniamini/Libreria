61739b62d712b66ff23a9dab678184ccdfdda3bc
ElisaBeniamini <elisaaa8783@gmail.com>
2023-11-20 15:14:00 +0100

    pagina create style

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -4,18 +4,8 @@
     </x-slot>
     <!-- Navigation-->
     <x-navbar />
-    <!--button go-back START-->
-    <div class="row  ">
-        <div class="col-12 col-md-12 d-flex  justify-content-center align-items-center">
-            <a class="text-decoration-none  text-dark " href="{{ URL::previous() }}">
-                <i class="bi bi-backspace-fill mx-2 text-dark fs-3"></i>
-            </a>
-            <h3 class="mt-2">Inserisci libro </h3>
 
-        </div>
 
-    </div>
-    <!--button go-back END-->
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -25,64 +15,95 @@
             </ul>
         </div>
     @endif
+    <!--button go-back  + title inserisci libro-->
+    <div class="div-title-create mt-3">
+        <a class="text-decoration-none  text-dark " href="{{ URL::previous() }}">
+            <i class=" mx-2 bi bi-backspace-fill text-dark fs-4"></i>
+        </a>
+        <h3>Inserisci libro </h3>
+    </div>
+    <div class=" container d-flex justify-content-center ">
+        <!--row form inserimento libro + img -->
+        <div class="row  mt-3">
+            <!--inserimento libro -->
+            <div class="col-12 col-md-6 ">
+                <form method="POST" action="{{ route('books.store') }}" enctype="multipart/form-data"
+                    class="mt-2 form-style-css">
+                    @csrf
+                    @method('POST')
+                    <div>
+                        <label for="name" class="form-label">Titolo</label>
+                        <input class="form-control" placeholder="" id="name" value="{{ old('name') }}"
+                            type="text" name="name">
+                        @error('name')
+                            <span style="color: red">
+                                Errore - nome obbligatorio!
+                            </span>
+                        @enderror
+                    </div>
+                    <div>
+                        <label for="name" class="form-label">Autore</label>
+                        <select name="author_id" class="form-control">
+                            <option value="" disabled selected>Scegli Autore</option>
+                            @foreach ($authors as $author)
+                                <option value="{{ $author->id }}">{{ $author->firstname . ' ' . $author->lastname }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="d-flex mt-3 px-1">
+                        <div>
+                            <label for="email" class="form-label ">Numero pagine</label>
+                            <input class="form-control" placeholder="xxx pagine" id="page"
+                                value="{{ old('pages') }}" type="text" name="pages">
+                            @error('pages')
+                                <span style="color: red">
+                                    Errore - pagine obbligatorio!
+                                </span>
+                            @enderror
+                        </div>
+                        <div class=" mx-1 ">
+                            <label for="email" class="form-label  ">Anno </label>
+                            <input class="form-control" placeholder="Pubblicato il ..." id="year"
+                                value="{{ old('year') }}" type="text" name="year">
+                            @error('year')
+                                <span style="color: red">
+                                    Errore - Anno obbligatorio!
+                                </span>
+                            @enderror
+                        </div>
 
 
-    <div class="d-flex justify-content-center ">
-        <form method="POST" action="{{ route('books.store') }}" enctype="multipart/form-data" class="mt-2">
-            @csrf
-            @method('POST')
-            <div class="mb-3 mt-4">
-                <input class="form-control" id="name" value="{{ old('name') }}" type="text" name="name">
-                <label for="name" class="form-label">Titolo</label>
-                @error('name')
-                    <span style="color: red">
-                        Errore - nome obbligatorio!
-                    </span>
-                @enderror
+                    </div>
+                    <div class="d-flex flex-column mt-3">
+
+
+                        <label for="email" class="form-label  ">Categoria </label>
+                        <select name="category_id" class="form-control">
+                            <option value="" disabled selected>Scegli Categoria</option>
+
+
+
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}"> {{ $category->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class=" mt-3 ">
+                        <label for="image" class="form-label">Copertina</label>
+                        <input class="form-control  bg-dark bg-gradient text-light" id="image" type="file"
+                            name="image">
+                    </div>
+                    <button type="submit" class=" button-30 mt-3 button-30-media">Aggiungi!</button>
+                </form>
             </div>
-
-            <select name="author_id" class="form-control">
-                @foreach ($authors as $author)
-                    <option value="{{ $author->id }}">{{ $author->firstname . ' ' . $author->lastname }}</option>
-                @endforeach
-            </select>
-            <label for="name" class="form-label">Autore</label>
-
-            <div>
-                <input class="form-control" id="page" value="{{ old('pages') }}" type="text" name="pages">
-                <label for="email" class="form-label">Numero pagine</label>
-                @error('pages')
-                    <span style="color: red">
-                        Errore - pagine obbligatorio!
-                    </span>
-                @enderror
+            <!--img inserimento libro -->
+            <div class="col-12 col-md-6 ">
+                <img width="100%" height="100%" class="img-fluid" src="\immagini\4087-removebg-preview.png"
+                    alt="">
             </div>
-            <div class="mb-3 ">
-                <input class="form-control" id="year" value="{{ old('year') }}" type="text" name="year">
-                <label for="email" class="form-label">Anno di pubblicazione</label>
-                @error('year')
-                    <span style="color: red">
-                        Errore - Anno obbligatorio!
-                    </span>
-                @enderror
-            </div>
-
-
-            @foreach ($categories as $category)
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="{{ $category->id }}" name="categories[]">
-                    <label class="form-check-label" for="flexCheckDefault">
-                        {{ $category->name }}
-                    </label>
-                </div>
-            @endforeach
-
-            <div class=" mt-3 ">
-                <input class="form-control" id="image" type="file" name="image">
-                <label for="image" class="form-label">Copertina</label>
-            </div>
-            <button type="submit" class="btn btn-outline-warning text-black">Aggiungi!</button>
-        </form>
+        </div>
     </div>
 
 
